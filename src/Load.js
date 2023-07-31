b1ddcf485884ee760f5012476fed7e0268b90f81
varsil suvagiya <varsilsuvagiya@gmail.com>
2023-07-31 20:38:00 +0530

    31-7-23

--- src/Load.js
+++ src/Load.js
@@ -14,8 +14,11 @@ loadjs("assets/plugins/bootstrap/js/wysihtml5-0.3.0.js",{async:true}),
 loadjs("assets/plugins/bootstrap/js/bootstrap-wysihtml5.js",{async:true}),
 loadjs("assets/plugins/aos-master/aos.js",{async:true}),
 loadjs("assets/plugins/nice-select/js/jquery.nice-select.min.js",{async:true}),
+
 loadjs("assets/js/custom.js",{async:true}),
 loadjs("assets/js/Extra.js",{async:true})
+
+
 
 // loadjs('assets.mycustom.js')
 
